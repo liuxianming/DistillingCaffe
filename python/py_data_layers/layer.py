aae3107e364be3ad881cf540a5657709c1a34fe3
liuxianming <liuxianming@gmail.com>
2015-08-03 10:37:13 -0500

    Changes to python data layers

--- python/py_data_layers/layer.py
+++ python/py_data_layers/layer.py
@@ -17,6 +17,8 @@ Load data from either datum or from original image
 Here, I used caffe.io.read_datum_from_image to resolve all format transformation,
 instead of the io functions provided by caffe python wrapper
 """
+
+
 def extract_sample_from_datum(datum, image_mean, resize=-1):
     # extract numpy array from datum, then substract the mean_image
     img_data = decode_datum(datum)
@@ -24,19 +26,23 @@ def extract_sample_from_datum(datum, image_mean, resize=-1):
     if resize == -1:
         return img_data
     else:
-        # resize image: first transfer back to h * w * c, -> resize -> c * h * w
+        # resize image:
+        # first transfer back to h * w * c, -> resize -> c * h * w
         resize_img_data(img_data, resize)
 
+
 def resize_img_data(img_data, resize):
-    img_data = img_data.transpose(1,2,0)
+    img_data = img_data.transpose(1, 2, 0)
     img_data = scipy.misc.imresize(img_data, (resize, resize))
-    return img_data.transpose(2,0,1)
+    return img_data.transpose(2, 0, 1)
+
 
 def decode_datum(datum):
-    if datum.encoded == True:
+    if datum.encoded is True:
         datum = caffe.io.decode_datum(datum)
     img_data = caffe.io.datum_to_array(datum)
     return img_data
+
 
 def extract_sample_from_image(image_fn, image_mean):
     # extract image data from image file directly,
@@ -50,18 +56,21 @@ def extract_sample_from_image(image_fn, image_mean):
 Substract image mean from data sample
 image_mean is a numpy array, either 1 * 3 or of the same size as input image
 """
+
+
 def substract_mean(img, image_mean):
     if image_mean.ndim == 1:
         image_mean = image_mean[:, np.newaxis, np.newaxis]
     img -= image_mean
     return img
 
-"""--------------------------------------------------------------------------"""
 
 """Expand_Data_Layer
 Expand compact labels / vectors into flat ones
 Example: from [1, 2, 4] ->[0, 1, 1, 0, 1, ...]
 """
+
+
 class ExpandLayer(caffe.Layer):
     """Expand compact labels for training / testing"""
 
@@ -86,7 +95,6 @@ class ExpandLayer(caffe.Layer):
         self._cur.first()
         print "Starting process for {}".format(self._db_name)
 
-
     def _get_mini_batch(self):
         batch = []
         for idx in range(self._batch_size):
@@ -100,9 +108,7 @@ class ExpandLayer(caffe.Layer):
         return batch
 
     def _get_next_minibatch(self):
-        #print 'ExpandLabelFetcher started...'
         batch = np.zeros(self._top_shape)
-        #print batch.shape
         mini_batch = self._get_mini_batch()
         if len(mini_batch) != self._top_shape[0]:
             print "Batch size can not match"
@@ -130,8 +136,11 @@ class ExpandLayer(caffe.Layer):
         top[0].reshape(top_shape[0], top_shape[1], top_shape[2], top_shape[3])
 
 """BClassificationDataLayer:
-Extract and prepare training / testing data from data / label lmdb for multi-class classification,
+Extract and prepare training / testing data
+from data / label lmdb for multi-class classification,
 """
+
+
 class MultiClassDataLayer(caffe.Layer):
     def setup(self, bottom, top):
         # add a filed named "param_str" in python_param, in prototxt
@@ -154,7 +163,8 @@ class MultiClassDataLayer(caffe.Layer):
             top[1].reshape(self._batch_size, 1, 1, 1)
             # fetch a datum from self._db to get size of images
             datum = self._get_a_datum(self._cur)
-            img_data  = extract_sample_from_datum(datum, self._mean, self._resize)
+            img_data = extract_sample_from_datum(datum, self._mean,
+                                                 self._resize)
             top[0].reshape(self._batch_size, *(img_data.shape))
             self._top_data_shape = top[0].data.shape
             self._top_label_shape = (self._batch_size, 1, 1, 1)
@@ -197,7 +207,8 @@ class MultiClassDataLayer(caffe.Layer):
         self._label_cur = self._label_db.begin().cursor()
         self._cur.first()
         self._label_cur.first()
-        print "Starting process for {} / {}".format(self._db_name, self._label_db_name)
+        print "Starting process for {} / {}".format(self._db_name,
+                                                    self._label_db_name)
 
     """preload_project_db:
     This function preload all datum into memory"""
@@ -251,11 +262,12 @@ class MultiClassDataLayer(caffe.Layer):
         # decode and return a tuple (data_batch, label_batch)
         for idx in range(self._batch_size):
             datum = self._data[self._pos]
-            img_data  = extract_sample_from_datum(datum, self._mean, self._resize)
+            img_data = extract_sample_from_datum(datum, self._mean,
+                                                 self._resize)
             batch[idx, ...] = img_data
             label_batch[idx, ...] = self._label[self._pos]
 
-            self._pos =( self._pos + 1) % self._n_samples
+            self._pos = (self._pos + 1) % self._n_samples
         end = time.time()
         print "Get a batch costs [{} seconds]".format(end-start)
         return (batch, label_batch)
@@ -266,8 +278,6 @@ class MultiClassDataLayer(caffe.Layer):
     def forward(self, bottom, top):
         start = time.time()
         blob, label_blob = self._get_next_minibatch()
-        #blob.reshape(*(top[0].data.shape))
-        #label_blob.reshape(*(top[1].shape))
         # by default, caffe use float instead of double
         top[0].data[...] = blob.astype(np.float32, copy=False)
         top[1].data[...] = label_blob.astype(np.float32, copy=False)
@@ -277,9 +287,10 @@ class MultiClassDataLayer(caffe.Layer):
     def backward(self, top, propagate_down, bottom):
         pass
 
+
 class PrefetchMultiClassDataLayer(caffe.Layer):
     def setup(self, bottom, top):
-        # add a filed named "param_str" in python_param, in prototxt
+        # add a field named "param_str" in python_param, in prototxt
         layer_params = yaml.load(self.param_str_)
         try:
             self._batch_size = int(layer_params['batch_size'])
@@ -305,18 +316,14 @@ class PrefetchMultiClassDataLayer(caffe.Layer):
             self._top_label_shape = (self._batch_size, 1, 1, 1)
             # then return the cursor to the initial position
             self._cur.first()
-            """
-            # prepare the prefetech process
-            self._blob_queue = Queue(30)
-            self._prefetch_process = BatchFetcher(self._blob_queue, self._cur, self._label_cur,
-                                                  self._mean_file, self._resize, self._top_data_shape)
-            """
             # using pipe() instead of queue to speed up
             self._conn, conn = Pipe()
-            self._prefetch_process = BatchFetcher(conn, self._cur, self._label_cur,
-                                                  self._mean_file, self._resize, self._top_data_shape)
+            self._prefetch_process = BatchFetcher(
+                conn, self._cur, self._label_cur,
+                self._mean_file, self._resize, self._top_data_shape)
             print "Start prefeteching process..."
             self._prefetch_process.start()
+
             def cleanup():
                 print 'Terminating BatchFetcher Process working in backend'
                 self._prefetch_process.terminate()
@@ -341,8 +348,8 @@ class PrefetchMultiClassDataLayer(caffe.Layer):
         self._label_cur = self._label_db.begin().cursor()
         self._cur.first()
         self._label_cur.first()
-        print "Starting process for {} / {}".format(self._db_name, self._label_db_name)
-
+        print "Starting process for {} / {}".format(self._db_name,
+                                                    self._label_db_name)
 
     def _get_next_minibatch(self):
         batch = self._conn.recv()
@@ -361,7 +368,8 @@ class PrefetchMultiClassDataLayer(caffe.Layer):
 
 
 class BatchFetcher(Process):
-    def __init__(self, conn, img_db_cursor, label_cursor, image_mean, resize, top_shape):
+    def __init__(self, conn, img_db_cursor, label_cursor,
+                 image_mean, resize, top_shape):
         super(BatchFetcher, self).__init__()
         self._conn = conn
         self._cur = img_db_cursor
@@ -375,11 +383,15 @@ class BatchFetcher(Process):
         self._resize = resize
         self._preload_db()
 
+    def _type(self):
+        return 'BatchFetcher'
+
     def _preload_db(self):
         self._preload_data()
         self._preload_label()
         self._n_samples = len(self._data)
         self._pos = 0
+        print 'Number of samples pre-loaded: {}'.format(self._n_samples)
 
     def _set_mean(self, image_mean):
         if type(image_mean) is str:
@@ -411,7 +423,8 @@ class BatchFetcher(Process):
             if not self._cur.next():
                 break
         end = time.time()
-        print "Preloading image data done [{} second]".format(end-start)
+        print "[{}]: Preloading image data done [{} second]".format(
+            self._type(), end - start)
 
     def _preload_label(self):
         # use the first label as output,
@@ -434,11 +447,12 @@ class BatchFetcher(Process):
         # decode and return a tuple (data_batch, label_batch)
         for idx in range(self._batch_size):
             datum = self._data[self._pos]
-            img_data  = extract_sample_from_datum(datum, self._mean, self._resize)
+            img_data = extract_sample_from_datum(
+                datum, self._mean, self._resize)
             batch[idx, ...] = img_data
             label_batch[idx, ...] = self._label[self._pos]
 
-            self._pos =( self._pos + 1) % self._n_samples
+            self._pos = (self._pos + 1) % self._n_samples
         return (batch, label_batch)
 
     def run(self):
@@ -449,8 +463,10 @@ class BatchFetcher(Process):
 
 """pairlayer.py
 This file implements python data layer for siamese / triplet training
-To improve efficiency, all layers use prefetching since sampling is time consuming
+To improve efficiency,
+all layers use prefetching since sampling is time consuming
 """
+
 
 class PrefetchSiameseLayer(caffe.Layer):
     def setup(self, bottom, top):
@@ -464,7 +480,7 @@ class PrefetchSiameseLayer(caffe.Layer):
             else:
                 self._resize = -1
 
-            print "Setting up python data layer"
+            print "Setting up python data layer: [type = PrefetchSiameseLayer]"
             self._mean_file = layer_params['mean_file']
             self._db_name = layer_params['source']
             self._set_up_db()
@@ -475,18 +491,22 @@ class PrefetchSiameseLayer(caffe.Layer):
             img_data = decode_datum(datum)
             if self._resize > 0:
                 img_data = resize_img_data(img_data, self._resize)
-            # top[0] should be of size (batch_size, channels * 2, height, width)
-            top[0].reshape(self._batch_size, img_data.shape[0]*2, img_data.shape[1], img_data.shape[2])
+            # top[0] should be of size
+            # (batch_size, channels * 2, height, width)
+            top[0].reshape(self._batch_size, img_data.shape[0]*2,
+                           img_data.shape[1], img_data.shape[2])
             self._top_data_shape = top[0].data.shape
             self._top_sim_shape = (self._batch_size, 1, 1, 1)
             # then return the cursor to the initial position
             self._cur.first()
             # using pipe() instead of queue to speed up
             self._conn, conn = Pipe()
-            self._prefetch_process = SiameseBatchFetcher(conn, self._cur, self._mean_file,
-                                                         self._resize, self._top_data_shape)
+            self._prefetch_process = SiameseBatchFetcher(
+                conn, self._cur, self._mean_file,
+                self._resize, self._top_data_shape)
             print "Start prefeteching process..."
             self._prefetch_process.start()
+
             def cleanup():
                 print 'Terminating prefetching process working in backend'
                 self._prefetch_process.terminate()
@@ -542,9 +562,13 @@ class SiameseBatchFetcher(Process):
         self._resize = resize
         self._preload_db()
 
+    def _type(self):
+        return 'SiameseBatchFetcher'
+
     def _preload_db(self):
         self._preload_data()
         self._n_samples = len(self._data)
+        print 'Total number of samples pre-loaded: {}'.format(self._n_samples)
 
     def _set_mean(self, image_mean):
         if type(image_mean) is str:
@@ -557,7 +581,6 @@ class SiameseBatchFetcher(Process):
                 blob_str = open(image_mean, 'rb').read()
                 blob.ParseFromString(blob_str)
                 self._mean = np.array(caffe.io.blobproto_to_array(blob))[0]
-            # self.mean = self.mean.transpose(1,2,0)
         else:
             self._mean = image_mean
         print "[Debug Info]: Image Mean Shape = {}".format(self._mean.shape)
@@ -578,7 +601,8 @@ class SiameseBatchFetcher(Process):
                 break
         self._label = np.asarray(self._label)
         end = time.time()
-        print "Preloading image data done [{} second]".format(end-start)
+        print "[{}]: Preloading image data done [{} second]".format(
+            self._type(), end - start)
 
     def _sampling(self, query_id=-1, positive=True):
         # sampling function: given query sample id
@@ -607,13 +631,15 @@ class SiameseBatchFetcher(Process):
             # generate positive pair and a negative pair for each query sample
             ref_type = [True, False]
             datum = self._data[q_id]
-            img_data  = extract_sample_from_datum(datum, self._mean, self._resize)
+            img_data = extract_sample_from_datum(
+                datum, self._mean, self._resize)
             for type_offset in range(2):
                 positive = ref_type[type_offset]
                 datum_offset = 2 * idx + type_offset
                 r_id = self._sampling(q_id, positive)
                 r_datum = self._data[r_id]
-                r_img_data = extract_sample_from_datum(r_datum, self._mean, self._resize)
+                r_img_data = extract_sample_from_datum(
+                    r_datum, self._mean, self._resize)
                 batch[datum_offset, ...] = np.vstack([img_data, r_img_data])
                 label_batch[datum_offset, ...] = 1 if positive else 0
         end = time.time()

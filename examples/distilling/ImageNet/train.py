df5242ce19ab82f2493c77e42d15636e89beeee2
Xianming Liu <liuxianming@gmail.com>
2015-10-06 10:45:04 -0500

    adding finetune

--- examples/distilling/ImageNet/train.py
+++ examples/distilling/ImageNet/train.py
@@ -5,6 +5,7 @@ sys.path.append(os.path.join(CAFFE_ROOT_DIR, 'python'))
 
 import caffe
 import py_data_layers
+import urllib
 
 if __name__ == "__main__":
     task = sys.argv[1]
@@ -13,8 +14,6 @@ if __name__ == "__main__":
     os.system("cp -f train_val.prototxt ./{}/".format(task))
     os.system("cp -f solver.prototxt ./{}/".format(task))
 
-    os.chdir(task)
-
     caffe.set_mode_gpu()
     if len(sys.argv) == 3:
         device_id = int(sys.argv[2])
@@ -22,5 +21,16 @@ if __name__ == "__main__":
         device_id = 0
     caffe.set_device(device_id)
 
+    if len(sys.arg) == 4:
+        # finetune
+        pretrained_model_fn = sys.argv[3]
+        if not os.path.exists(pretrained_model_fn):
+            # download the model
+            url = "http://dl.caffe.berkeleyvision.org/bvlc_alexnet.caffemodel"
+            urllib.urlretrieve(url, pretrained_model_fn)
+
+    os.chdir(task)
     solver = caffe.SGDSolver('solver.prototxt')
+    if pretrained_model_fn:
+        solver.net.copy_from(pretrained_model_fn)
     solver.solve()
